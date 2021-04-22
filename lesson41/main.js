7ef08352e403d885a9c08222fc82b73ecb199406
Bogdan122kr <74065966+Bogdan122kr@users.noreply.github.com>
2021-04-22 23:35:38 +0300

    Add files via upload

---
+++ lesson41/main.js
@@ -0,0 +1,96 @@
+// let a = prompt("введите число 1")
+// switch(a==0){
+//     case true:alert("верно");break;
+//     default: alert("неверно")
+// }
+// let x = prompt("введите число 2")
+// switch(x>0){
+//     case true:alert("верно");break;
+//     default: alert("неверно")
+// }
+// let y = prompt("введите число 3")
+// switch(y<0){
+//     case true:alert("верно");break;
+//     default: alert("неверно")
+// }
+// let c = prompt("введите число 4")
+// switch(c>-1){
+//     case true: alert("верно");break;
+//     default: alert("неверно")
+// }
+// let h = prompt("введите число 5")
+// switch(h<1){
+//     case true: alert("верно");break;
+//     default: alert("неверно")
+// }
+// let j = prompt("введите число 6")
+// switch(j!=0){
+//     case true: alert("верно");break;
+//     default: alert("неверно")
+// }
+// let d = prompt("введите число 7")
+// switch(d=="test"){
+//     case true: alert("верно");break;
+//     default: alert("неверно")
+// }
+// let u = prompt("введите число 8")
+// switch(u==1|| u=="`1`"){
+//     case true:alert("верно");break;
+//     default:alert("неверно")
+// }
+// let a = prompt("введите число 1")
+// if(a==0){
+//     alert("verno")
+// }
+// else{
+//     alert("neverno")
+// }
+// let y = prompt("введите число 2")
+// if(y>0){
+//     alert("verno")
+// }
+// else{
+//     alert("neverno")
+// }
+// let t = prompt("введите число 3")
+// if(t<0){
+//     alert("verno")
+// }
+// else{
+//     alert("neverno")
+// }
+// let r = prompt("введите число 4")
+// if(r>-1){
+//     alert("verno")
+// }
+// else{
+//     alert("neverno")
+// }
+// let u =prompt("введите число 5")
+// if (u<-1){
+//     alert("verno")
+// }
+// else{
+//     alert("neverno")
+// }
+// let i = prompt("введите число 6")
+// if (i!=0){
+//     alert("verno")
+// }
+// else{
+//     alert("neverno")
+// }
+let g = prompt("введите число 7")
+if (g=="test"){
+    alert("verno")
+}
+else{
+    alert("neverno")
+}
+let v = prompt("введите число 8")
+if(v==1|| v=="`1`"){
+    alert("verno")
+}
+else{
+    alert("neverno")
+}
---
+++ lesson43/main.js
@@ -0,0 +1,47 @@
+// let x = 1
+// for(x=1;x<101;x++){
+//     console.log(x)
+// }
+
+// let y = 100
+// for(y=100;y>0;y--){
+//     console.log(y)
+// }
+// let c = 2
+//  for(c=2;c<101;c+=2){
+//      document.write(c)
+//      console.log(c)
+//  }
+//  let arr = []
+//     for(i=0;i<10;i++){
+//     arr[i]="x"    
+// }
+//     console.log(arr)
+//     document.write(arr)
+// let arr = []
+// for (i=1;i<11;i++){
+// arr.push(i)
+// }
+// console.log(arr)
+// document.write(arr)
+
+//  let arr = []
+//  for (i=1;i<10;i++){
+//  arr.push(Math.random().toFixed(2))
+//  }
+//  console.log(arr)
+
+//   let arr = [1,2,3,4,5,6,7,8,9,10]
+//   for (i=1;i<10;i++){
+//   if(arr[i]==5){
+//       alert("est")
+//   }
+//   }
+
+  let arr = [1,2,3,4,5,6,7,8,9,10]
+  a=0
+  for(i=0;i<10;i++){
+a+= arr[i]
+a+=2
+}
+console.log(a)
