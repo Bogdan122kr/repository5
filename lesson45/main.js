397ab056ebf6d622aa17741e8427fa28af4194a5
Bogdan122kr <74065966+Bogdan122kr@users.noreply.github.com>
2021-05-21 16:40:58 +0300

    Add files via upload

---
+++ lesson45/main.js
@@ -0,0 +1,48 @@
+// function showMessege(){
+//     alert("привет Саша")
+// }
+// showMessege();
+
+
+
+
+// function showMessege(){
+//     let a = "Привет Мир"
+//     alert(a)
+// }
+// showMessege();
+
+// function showMessege(){
+//     let a = prompt("Как тебя зовут?")
+//     alert(a)
+// }
+// showMessege();
+
+// let MyName = prompt("Как тебя зовут?")
+// function showMessege(){
+//     let a = "Привет" + " " + MyName
+//     alert(a)
+// }
+// showMessege();
+
+
+// function showMessage(from,text){
+// alert( from + " " + "polushit" + " " + text)
+// }
+// showMessage("sasha" + " " + "diplom")
+
+// function showMessage(a, b){
+//     alert(a + " " + "геркулес ел кашу" + " " +b)
+// }
+// let a = prompt("как ел геркулес ел кашу?")
+//     b = prompt("где?")
+// showMessage(a,b)    
+
+
+// let  a = Number(prompt( "a="))
+//      b = Number(prompt("b="))
+// function sum (a, b){
+//     return a + b
+// }
+// let c = sum(a,b)
+// alert(c);
